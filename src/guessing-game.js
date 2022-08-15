20ae8b1c109235accf13d486d8deec7687c854c8
sneguroma <sneguroma@gmail.com>
2022-08-15 19:20:53 +0300

    feat: ready to push

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
-class GuessingGame {
-    constructor() {}
-
+class GuessingGame {     
+    constructor() {      
+    }
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      this.middle = Math.round((this.max- this.min) / 2 + this.min);
+      return this.middle;
     }
 
     lower() {
-
+      this.max = this.middle;      
     }
 
     greater() {
-
+      this.min = this.middle;       
     }
 }
 
